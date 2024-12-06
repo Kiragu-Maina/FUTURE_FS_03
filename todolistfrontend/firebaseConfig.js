900dd2e13a2a837566ad6e35792bef86e732e8a8
Kiragu Maina <jumaliko711@gmail.com>
2024-12-06 10:14:39 +0300

    Update firebaseConfig.js

--- todolistfrontend/firebaseConfig.js
+++ todolistfrontend/firebaseConfig.js
@@ -1,12 +1,6 @@
 
 const firebaseConfig = {
-  apiKey: "xxxx",
-  authDomain: "xxxx",
-  projectId: "xxxx",
-  storageBucket: "xxxx",
-  messagingSenderId: "xxxx",
-  appId: "xxxx",
-  measurementId: "xxxx"
+
 };
 
   
